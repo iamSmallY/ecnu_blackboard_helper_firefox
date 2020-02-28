7d97ac9c011adc0c039bfd74f7ec681932e10f21
smally <2907047490@qq.com>
2020-02-28 11:34:17 +0800

    fix the bug that some files have no suffix name

--- background.js
+++ background.js
@@ -10,18 +10,19 @@ browser.runtime.onMessage.addListener(
     function (arg, sender, sendResponse) {
         var dllist = arg;
         for (const file of dllist) {
-            if (file[0].indexOf("https://elearning.ecnu.edu.cn/") != -1) {
+            if (file[1].indexOf("pdf") != -1 || file[1].indexOf("PDF") != -1){
+                file[0] += ".pdf";
                 browser.downloads.download({
-                    url: file[0]
+                    url: file[0],
+                    saveAs: true
                 });
             } else {
                 browser.downloads.download({
                     url: file[0],
                     filename: file[1],
                     saveAs: true
-                });
+                })
             }
-
         }
     }
 );
